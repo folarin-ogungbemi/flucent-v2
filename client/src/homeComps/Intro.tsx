68c09629137f85075730cb40ab5747c64df79495
folarin-ogungbemi <ftogungbemi@gmail.com>
2023-08-28 07:51:49 +0200

    Create Intro aside in Home component

--- client/src/homeComps/Intro.tsx
+++ client/src/homeComps/Intro.tsx
@@ -1,18 +1,15 @@
 import React from 'react'
-import wave from '../assets/images/waving-hand-medium-skin-tone-svgrepo-com.svg'
+// import wave from '../assets/images/waving-hand-medium-skin-tone-svgrepo-com.svg'
 
 const Intro:React.FC = () => {
   return (
-   <div className='max-w-[1400px] mx-auto p-5'>
-      <div className='flex py-5 px-10 shadow-lg shadow-[#55555580] h-fit w-fit rounded-lg space-x-5'>
-          <img src={wave} height={2.5} width={45} alt="Hello wave" />
-          <div className='flex flex-col'>
-              <span className='text-sm'>Hi, I am</span><span className=' font-medium text-3xl'>Folarin</span>
-          </div>
-      </div>
-      <section className="mt-5">
-        <h2 className="text-2xl">Fullstack Software Developer</h2>
-        <p>I am a software engineer with a focus on utilizing Python simplicity and harnessing Javascript's versatility to leverage technology.</p>
+    <div className='h-fit 2xl:w-[80%] mt-20'>
+      <h2 className='font-bold text-4xl mb-3'>
+        <span className='bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500'>Folarin Ogungbemi</span>
+      </h2>
+      <section className="space-y-2">
+        <h3 className="text-2xl font-medium">Fullstack Software Developer</h3>
+        <p className='w-[80%] text-skin-fontSm font-light text-sm'>I am a software developer with a focus on utilizing Python simplicity and harnessing Javascript's versatility to leverage technology.</p>
       </section>
     </div>
   )
