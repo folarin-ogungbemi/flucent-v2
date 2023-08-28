80bd3fefd5b85d7232bc20de1f64ee55e7e07ac7
folarin-ogungbemi <ftogungbemi@gmail.com>
2023-08-28 07:47:38 +0200

    Create switch mode

---
+++ client/src/components/SwitchMode.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import {BiSolidMoon, BiSolidSun} from 'react-icons/bi'
+
+const SwitchMode = () => {
+
+  const setDarkMode = () =>{
+    document.querySelector('html')?.setAttribute('data-theme', 'dark')
+  }
+
+  const setLightMode = () =>{
+    document.querySelector('html')?.setAttribute('data-theme', 'light')
+  }
+
+  const [mode, setMode] = React.useState(false)
+
+  const handleModeState = () =>{
+    setMode(prevState => !prevState)
+    
+  }
+
+  return (
+    <div className='flex space-x-2 text-sm my-auto'> {!mode ?<span>Light mode </span>: <span>Dark mode</span>}
+      <div className="w-10 h-4 bg-slate-500 rounded-full relative cursor-pointer my-auto drop-shadow-md transition-all ease-in-out delay-200" onClick={handleModeState}>
+        {!mode ? <>{setLightMode()}<span className="w-4 h-4 bg-[#f6ba00] rounded-full absolute inset-0"></span> <BiSolidSun className="w-3 h-3 text-[#fff] absolute right-1.5 top-[50%] translate-y-[-50%]"/></>
+         : <>{setDarkMode()}<span className="w-4 h-4 bg-[#fff] rounded-full absolute right-0 top-0"></span> <BiSolidMoon className="w-3 h-3 text-[#000] absolute left-1.5 top-[50%] translate-y-[-50%]"/></>}
+      </div>
+    </div>
+  )
+}
+
+export default SwitchMode;
