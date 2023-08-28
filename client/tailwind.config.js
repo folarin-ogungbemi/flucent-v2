54d4be8a7feaaf8de24fb6d2f930dcc9831e26f2
folarin-ogungbemi <ftogungbemi@gmail.com>
2023-08-28 07:59:27 +0200

    configure style variables

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,28 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      textColor:{
+        skin:{
+          fontSm:'var(--font-sm-color)',
+          fontMd:'var(--font-md-color)',
+          oFont:'var(--o-font-color)',
+          link:'var(--link-color)',
+        }
+      },
+      backgroundColor:{
+        skin:{
+          fill:'var(--fill-color)',
+          oFill:'var( --o-fill-color)',
+          fillAside:'var(--fill-aside-color)',
+        }
+      },
+      boxShadow:{
+        skin:{
+          shadow:'var(--shadow-color)',
+        }
+      },
+    },
   },
   plugins: [],
 }
