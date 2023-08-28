2880271607d46555d833a24c9c77f2645f9f3514
folarin-ogungbemi <ftogungbemi@gmail.com>
2023-08-28 07:49:40 +0200

    Create contact links

--- client/src/homeComps/ContactLinks.tsx
+++ client/src/homeComps/ContactLinks.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from 'react'
-// import {Link} from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 import {FaGithub, FaLinkedin, FaTwitter} from 'react-icons/fa'
 import {MdEmail} from 'react-icons/md'
 
@@ -7,32 +7,37 @@ const ContactLinks:React.FC= () => {
 
     interface IContact{
         name:ReactNode;
+        color: string
         url:string;
     }
 
     const contacts:Array<IContact> = [
         {
             name: <FaGithub/>,
+            color: '#4078c0',
             url:'https://github.com/folarin-ogungbemi'
         },
         {
             name: <FaLinkedin/>,
+            color: '#0e76a8',
             url:'https://www.linkedin.com/in/folarin-ogungbemi/'
         },
         {
             name: <FaTwitter/>,
+            color: '#1da1f2',
             url:'https://twitter.com/ftogungbemi'
         },
         {
             name: <MdEmail/>,
+            color: '#4285f4',
             url:'mailto:ftogungbemi@gmail.com'
         },
     ]
   return (
-    <ul className='flex justify-between w-[50%] mx-auto '>
+    <ul className='flex gap-4 '>
         {contacts.map((contact, contactIndex) => 
-        <li key={contactIndex}>
-            <div className='text-2xl'>{contact.name}</div>
+        <li key={contactIndex} className='transition delay-100 ease-in-out hover:-translate-y-2'>
+            <NavLink to={contact.url} className='text-2xl cursor-pointer' style={{ color: contact.color }}>{contact.name}</NavLink>
         </li>
         )}
     </ul>
