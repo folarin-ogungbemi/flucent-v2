18688d8e3ed950aeeb9f7fe52ed2adc6727cfa39
folarin-ogungbemi <ftogungbemi@gmail.com>
2023-08-28 07:50:26 +0200

    Update page layout

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from 'react'
 import {Helmet} from "react-helmet";
+import ContactLinks from '../homeComps/ContactLinks'
 
 interface LayoutProps{
     title: string;
@@ -7,16 +8,19 @@ interface LayoutProps{
     children: ReactNode;
 }
 
-const Layout = ({title, content, children}: LayoutProps) => {
+const Layout:React.FC<LayoutProps> = ({title, content, children}) => {
   return (
-    <>
+    <div className='realtive h-fit'>
         <Helmet>
             <title>{title}</title>
             <meta name='description' content={content} />
         </Helmet>
         <div>{children}</div>
-    </>
+        <div className='fixed bottom-[5%] left-[50%] translate-x-[-50%] bg-[#F5F5DC0d] backdrop-blur-lg px-5 py-2 shadow-md shadow-skin-shadow rounded-full'>
+          <ContactLinks/>
+        </div>
+    </div>
   )
 }
-
-export default Layout
+ 
+export default Layout;
