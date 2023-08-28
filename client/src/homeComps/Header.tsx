763eeccf8ca01edcdd7c846a67f5b64cd5b5c2a0
folarin-ogungbemi <ftogungbemi@gmail.com>
2023-08-28 17:19:35 +0200

    Add Navbar on scroll functionality

--- client/src/homeComps/Header.tsx
+++ client/src/homeComps/Header.tsx
@@ -4,8 +4,33 @@ import Nav from './Nav'
 import SwitchMode from '../components/SwitchMode'
 
 const Header:React.FC = () => {
+
+  const [prevScrollPos, setPrevScrollPos] = React.useState(0);
+  const [visible, setVisible] = React.useState<boolean>(true);
+
+  React.useEffect(() =>{
+
+    const handleScroll = () => {
+    const currentScrollPos = window.pageYOffset;
+    const navBar = document.getElementsByTagName('header')[0];
+
+    (prevScrollPos > currentScrollPos || currentScrollPos === 0) ? (setVisible(true), navBar.style.top = '0')
+    : (setVisible(false), navBar.style.top = `-${navBar.offsetHeight}px`)
+      setPrevScrollPos(currentScrollPos);
+
+      currentScrollPos === 0 ? (navBar.style.boxShadow = 'none', navBar.style.background = 'transparent')
+      : (navBar.style.boxShadow = '0px 0.1px 3px 0.5px #0000001a', navBar.style.background = '#fff')
+    }
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+    
+  }, [prevScrollPos, visible]);
+
   return (
-    <div className='flex gap-2 shadow shadow-skin-shadow h-fit w-full p-5 z-20 drop-shadow-md'>
+    <header className='flex gap-2 fixed h-fit w-full max-w-[1400px] p-5 z-20 transition-all delay-200 duration-500 ease-in-out'>
       <div className='block w-[35%]'>
         <Logo/>
       </div>
@@ -13,7 +38,7 @@ const Header:React.FC = () => {
         <Nav/>
         <SwitchMode/>
       </div>
-    </div>
+    </header>
   )
 }
 
