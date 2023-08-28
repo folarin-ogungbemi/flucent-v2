d929209483c6074dd4c5ddc3921d237212cfbd0a
folarin-ogungbemi <ftogungbemi@gmail.com>
2023-08-28 07:45:50 +0200

    Update nav component

--- client/src/homeComps/Nav.tsx
+++ client/src/homeComps/Nav.tsx
@@ -1,15 +1,14 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {Link} from 'react-scroll'
 
 const Nav:React.FC = () => {
 
-  const navLinks:Array<string> = ['about', 'skills', 'experience', 'projects']
-  console.log(navLinks);
+  const navLinks:Array<string> = ['about', 'projects', 'experiences', ]
 
   return (
-    <nav className='flex flex-col space-y-3'>
+    <nav className='flex space-x-5 cursor-pointer'>
       {navLinks.map((nav, navIndex)=>
-        <Link to={`/${nav}`} key={navIndex} className='capitalize'>{nav}</Link>
+        <Link to={nav} activeClass="active" spy={true} smooth={true} offset={-85} duration={500} key={navIndex} className='nav-link text-md text-skin-link capitalize w-fit hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-r hover:from-pink-500 hover:to-violet-500'>{nav}</Link>
       )}
     </nav>
   )
