d378f21219dec5ec24b3d7bb02672d399131da20
folarin-ogungbemi <ftogungbemi@gmail.com>
2023-08-28 07:43:49 +0200

    Create skills data

---
+++ client/src/components/SkillsData.tsx
@@ -0,0 +1,183 @@
+interface ITechnology{
+    name:string;
+    url?:string;
+}
+
+export const frontendT:Array<ITechnology> = [
+    {
+        name: 'Javascript',
+        url: 'https://skillicons.dev/icons?i=js',
+    },
+    {
+        name: 'Typescript',
+        url: 'https://skillicons.dev/icons?i=ts',
+    },
+    {
+        name: 'React',
+        url: 'https://skillicons.dev/icons?i=react',
+    },
+    {
+        name: 'Redux',
+        url: 'https://skillicons.dev/icons?i=redux',
+    },
+    {
+        name: 'HTML',
+        url: 'https://skillicons.dev/icons?i=html',
+    },
+    {
+        name: 'CSS',
+        url: 'https://skillicons.dev/icons?i=css',
+    },
+    {
+        name: 'Bootstrap',
+        url: 'https://skillicons.dev/icons?i=bootstrap',
+    },
+     {
+        name: 'TailwindCSS',
+        url: 'https://skillicons.dev/icons?i=tailwind',
+    },
+]
+
+export const backendT:Array<ITechnology> = [
+    {
+        name: 'Python',
+        url: 'https://skillicons.dev/icons?i=py',
+    },
+    {
+        name: 'Django',
+        url: 'https://skillicons.dev/icons?i=django',
+    },
+    {
+        name: 'Flask',
+        url: 'https://skillicons.dev/icons?i=flask',
+    },
+    {
+        name: 'DRF',
+        url: '',
+    },
+    {
+        name: 'JWT',
+        url: '',
+    },
+    {
+        name: 'Stripe',
+        url: '',
+    },
+]
+
+export const dataBaseT:Array<ITechnology> = [
+     {
+        name: 'SQL',
+        url: 'https://skillicons.dev/icons?i=mysql',
+    },
+    {
+        name: 'PostgreSQL',
+        url: 'https://skillicons.dev/icons?i=postgres',
+    },
+    {
+        name: 'ElephantSQL',
+        url: 'https://skillicons.dev/icons?i=postgres',
+    },
+    {
+        name: 'redis',
+        url: 'https://skillicons.dev/icons?i=redis',
+    },
+]
+
+export const otherT:Array<ITechnology> = [
+    {
+        name: 'VSCode',
+        url: 'https://skillicons.dev/icons?i=vscode',
+    },
+    {
+        name: 'Vim',
+        url: 'https://skillicons.dev/icons?i=vim',
+    },
+    {
+        name: 'Linux',
+        url: 'https://skillicons.dev/icons?i=linux',
+    },
+    {
+        name: 'Figma',
+        url: 'https://skillicons.dev/icons?i=figma',
+    },
+    {
+        name: 'Ai',
+        url: 'https://skillicons.dev/icons?i=ai',
+    },
+    {
+        name: 'Vite',
+        url: 'https://skillicons.dev/icons?i=vite',
+    },
+    
+]
+
+export const versionControl_cloudServicesT:Array<ITechnology> = [
+    {
+        name: 'Selenium',
+        url: 'https://skillicons.dev/icons?i=selenium',
+    },
+    {
+        name: 'Jest',
+        url: 'https://skillicons.dev/icons?i=jest',
+    },
+    {
+        name: 'GitHub',
+        url: 'https://skillicons.dev/icons?i=github',
+    },
+    {
+        name: 'Git',
+        url: 'https://skillicons.dev/icons?i=git',
+    },
+    {
+        name: 'Heroku',
+        url: 'https://skillicons.dev/icons?i=heroku',
+    },
+    {
+        name: 'AWS S3',
+        url: 'https://skillicons.dev/icons?i=aws',
+    },
+    {
+        name: 'Cloudinary',
+        url: 'https://skillicons.dev/icons?i=aws',
+    },
+    {
+        name: 'Docker',
+        url: 'https://skillicons.dev/icons?i=docker',
+    },
+]
+
+export const dataAnalysisT:Array<ITechnology> = [
+    {
+        name: 'Python',
+        url: 'https://skillicons.dev/icons?i=py',
+    },
+    {
+        name: 'Excel',
+        url: 'https://skillicons.dev/icons?i=py',
+    },
+    {
+        name: 'PowerBI',
+        url: '',
+    },
+    {
+        name: 'Jupyter',
+        url: '',
+    },
+    {
+        name: 'Numpy',
+        url: '',
+    },
+    {
+        name: 'Matplotlib',
+        url: '',
+    },
+    {
+        name: 'Pandas',
+        url: '',
+    },
+    {
+        name: 'Seaborn',
+        url: '',
+    },
+]
