0c071b6ffbdf9bb98480847b1acbfa5e4c9550c1
folarin-ogungbemi <ftogungbemi@gmail.com>
2023-10-01 19:46:23 +0200

    Add switch function and style

--- client/src/components/SwitchMode.jsx
+++ client/src/components/SwitchMode.jsx
@@ -19,10 +19,13 @@ const SwitchMode = () => {
   }
 
   return (
-    <div className='flex space-x-2 text-sm my-auto'> {!mode ?<span>Light mode </span>: <span>Dark mode</span>}
+    <div className='flex space-x-2 text-sm my-auto switch-in-transition'> {!mode ?<span>Light mode </span>: <span>Dark mode</span>}
       <div className="w-10 h-4 bg-slate-500 rounded-full relative cursor-pointer my-auto drop-shadow-md transition-all ease-in-out delay-200" onClick={handleModeState}>
-        {!mode ? <>{setLightMode()}<span className="w-4 h-4 bg-[#f6ba00] rounded-full absolute inset-0"></span> <BiSolidSun className="w-3 h-3 text-[#fff] absolute right-1.5 top-[50%] translate-y-[-50%]"/></>
-         : <>{setDarkMode()}<span className="w-4 h-4 bg-[#fff] rounded-full absolute right-0 top-0"></span> <BiSolidMoon className="w-3 h-3 text-[#000] absolute left-1.5 top-[50%] translate-y-[-50%]"/></>}
+        {!mode ? <>{setLightMode()}<span className="w-3 h-3 bg-[#f6ba00] rounded-full absolute left-1 top-[50%] translate-y-[-50%]"></span>
+          <BiSolidSun className="w-3 h-3 text-[#fff] absolute right-1.5 top-[50%] translate-y-[-50%]"/></>
+         : <>{setDarkMode()}<span className="w-3 h-3 bg-[#fff] rounded-full absolute right-1 top-[50%] translate-y-[-50%]"></span>
+          <BiSolidMoon className="w-3 h-3 text-[#000] absolute left-1.5 top-[50%] translate-y-[-50%]"/></>
+        }
       </div>
     </div>
   )
